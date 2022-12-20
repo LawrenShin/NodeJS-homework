9e783b2423fa41fca42ab5931bfc64fe904b18c2
LawrenShin <simplyforgit@gmail.com>
2022-12-20 15:18:47 +0300

    comments domains are going to be deprecated but such a small task it is okay to use according to docs

--- 1_2.js
+++ 1_2.js
@@ -1,12 +1,14 @@
 const fs = require('fs');
 const csv = require('csvtojson');
+const domain = require('domain');
 
 const file = './csv/nodejs-hw1-ex1.csv';
+const d = domain.create();
 
 const readable = fs.createReadStream(file);
 const writable = fs.createWriteStream('parsed.txt');
 
-
-readable
-    .pipe(csv())
-    .pipe(writable);
+d.on('error', e => {
+    console.log(e, 'An error occured');
+});
+d.run(() => readable.pipe(csv()).pipe(writable));
