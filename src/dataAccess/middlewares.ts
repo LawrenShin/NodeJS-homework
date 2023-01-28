ad2ccfb2da39594c81fc06a32f01f689f34846be
Fedai Paca <fedaipaca@gmail.com>
2023-01-28 15:45:50 +0300

    fix: return response on deletion

--- src/dataAccess/middlewares.ts
+++ src/dataAccess/middlewares.ts
@@ -1,6 +1,6 @@
-import {Request, Response} from "express";
+import { Request, Response } from "express";
 import Joi from "joi";
-import {IUserBasic} from "../types";
+import { IUserBasic } from "../types";
 import {
     handleUpdateUser,
     handleUserCreation,
@@ -10,7 +10,7 @@ import {
     userGetter,
     validateUser
 } from "../services";
-import {User} from "../models";
+import { User } from "../models";
 
 const NotFoundByIDMessage = `User with this id not found`;
 
@@ -20,7 +20,7 @@ export const getUser = async (req: Request, res: Response) => {
     if (user) {
         res.json(noServiceFields(user));
     } else {
-        res.status(404).json({ error: 'User not found.' })
+        res.status(404).json({ error: 'User not found.' });
     }
 };
 
@@ -34,9 +34,9 @@ export const addUser = async (req: Request, res: Response) => {
         // Note: Got lost here in types, not sure how to handle properly =(
         const result = await handleUserCreation(validated.value as unknown as IUserBasic);
         if (result) {
-            res.json({result: noServiceFields(result), status: 201})
+            res.json({ result: noServiceFields(result), status: 201 });
         } else {
-            res.json({message: 'User already exist', status: 400});
+            res.json({ message: 'User already exist', status: 400 });
         }
     }
 };
@@ -44,9 +44,9 @@ export const addUser = async (req: Request, res: Response) => {
 export const getUserList = async (req: Request, res: Response) => {
     const userList = await userGetter();
 
-    if(Array.isArray(userList)) {
+    if (Array.isArray(userList)) {
         if (!userList.length) {
-            res.json({message: 'Database is empty.'});
+            res.json({ message: 'Database is empty.' });
         }
         res.json(noServiceFields(userList));
     }
@@ -56,25 +56,28 @@ export const getUserList = async (req: Request, res: Response) => {
 // But since sequelize has this out of box, it makes me feel like I am inventing the wheel instead of using default behavior of sequelize.
 // Thus, I am using my own wheel with Model.update() instead of Model.drop()
 export const removeUser = async (req: Request, res: Response) => {
-    const {id} = req.params;
+    const { id } = req.params;
     const user = await userGetter(id);
 
     if (!user) {
-        res.json({message: NotFoundByIDMessage, status: 400});
+        res.json({ message: NotFoundByIDMessage, status: 400 });
     } else {
-        return await deleteUser(id);
+        // you should send reponse, otherwise it hangs
+        const result = await deleteUser(id);
+        console.log(result);
+        res.status(204).json();
     }
 };
 
 export const updateUser = async (req: Request, res: Response) => {
-    const {id} = req.params;
+    const { id } = req.params;
     const updates = req.body;
     const result = await handleUpdateUser(id, updates);
     // Note: I rly don't like these responses - [1/0]. What would be better to have here though?
     if (!result[0]) {
-        res.json({message: NotFoundByIDMessage});
+        res.json({ message: NotFoundByIDMessage });
     } else {
-        res.json({message: `User has been updated`});
+        res.json({ message: `User has been updated` });
     }
 };
 
@@ -82,7 +85,7 @@ export const getUserListByName = async (req: Request, res: Response) => {
     const { login } = req.params;
     const { limit } = req.query;
     if (!login) {
-        res.json({message: 'Login can not be empty'});
+        res.json({ message: 'Login can not be empty' });
     } else {
         // Note: also here not sure regarding type
         const list = await handleUserListByName(login, limit as any);
@@ -90,11 +93,11 @@ export const getUserListByName = async (req: Request, res: Response) => {
     }
 };
 
-export const deleteUser = async (id: string): Promise<{ message: string }> => {
-    const result = await User.update({ isdeleted: true }, {where: {id: id}});
+export const deleteUser = async (id: string): Promise<{ message: string; }> => {
+    const result = await User.update({ isdeleted: true }, { where: { id: id } });
     if (result) {
-        return {message: `User has been deleted`};
+        return { message: `User has been deleted` };
     } else {
-        return {message: `Something went wrong)`};
+        return { message: `Something went wrong)` };
     }
 };
