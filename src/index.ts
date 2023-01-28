5648b17ea5b962ffac8984fa330e0837e175e337
Fedai Paca <fedaipaca@gmail.com>
2023-01-28 16:07:52 +0300

    refactor: move routing logic to routes folder

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import {sequelize} from "./connection";
 import {middlewares} from "./dataAccess";
+import { appRouter } from './routes';
 
 export const app = express();
-export const router = express.Router();
+// export const router = express.Router();
 export const port = process.env.PORT || 3000;
 
 sequelize.authenticate()
@@ -19,30 +20,33 @@ sequelize.sync().then(() => {
     console.log("All models were synchronized successfully.");
 });
 
-const {
-    getUserListByName,
-    removeUser,
-    updateUser,
-    getUser,
-    getUserList,
-    addUser,
-} = middlewares;
+// const {
+//     getUserListByName,
+//     removeUser,
+//     updateUser,
+//     getUser,
+//     getUserList,
+//     addUser,
+// } = middlewares;
 
-// API by id
-router.route('/users/:id')
-    .get(getUser)
-    .delete(removeUser)
-    .patch(updateUser);
 
-// API list
-router.route('/users')
-    .post(addUser)
-    .get(getUserList);
-router.route('/users/byName/:login')
-    .get(getUserListByName);
+// // API by id
+// router.route('/users/:id')
+//     .get(getUser)
+//     .delete(removeUser)
+//     .patch(updateUser);
+
+// // API list
+// router.route('/users')
+//     .post(addUser)
+//     .get(getUserList);
+// router.route('/users/byName/:login')
+//     .get(getUserListByName);
+
 
 app.use(express.json());
-app.use(router);
+// app.use(router);
+app.use(appRouter) // using seperated router in routes folder will help you clean up here.
 app.listen(port);
 
 // Note: Sorry, as I am running late for homeworks this one is a little bit unfinished with splitting
---
+++ src/routes/users.ts
@@ -0,0 +1,15 @@
+// Create a route file for each resource to be exposed, in this case /users
+// with this you seperate your routing logic for each resource (endpoint) to related and seperate files
+import express from "express";
+import { addUser, getUserList } from "../dataAccess/middlewares";
+
+const usersRouter = express.Router();
+
+usersRouter.route("/")
+    .post(addUser)
+    .get(getUserList);
+
+// i did a sample above. it is working but you should add others. like below
+// usersRouter.route("/:id").get(getUserById)
+
+export { usersRouter };
